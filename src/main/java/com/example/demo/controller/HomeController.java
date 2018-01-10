118037767db0f98f223528f921acd13d1370dfeb
ramendushandilya <ramendu.shandilya@gmail.com>
2018-01-10 18:29:33 +0530

    Initial commit

---
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -0,0 +1,20 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author rams0516
+ *         Date: 1/10/2018
+ *         Time: 5:38 PM
+ */
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/")
+    public String showHome() {
+        return "index";
+    }
+
+}
